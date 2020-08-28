9a1f0b30080aeab3888f4831dbbdaff5a4080ac7
Nguyen Ngoc Long <kenmerzy@gmail.com>
2020-08-28 16:35:49 +0700

    update lan 2

--- NguyenNgocLong/designInstagram/assets/images/index.js
+++ NguyenNgocLong/designInstagram/assets/images/index.js
@@ -9,15 +9,25 @@ const story3 = require('./story3.png')
 const story4 = require('./story4.png')
 const iconMore = require('./iconMore.png')
 const imageUpload = require('./imageUpload.png')
+const addButton = require('./addButton.png')
 const iconSave = require('./iconSave.png')
 const iconNumberOfPictures = require('./iconNumberOfPictures.png')
 const iconHeart = require('./iconHeart.png')
 const iconComment = require('./iconComment.png')
 const iconSlide = require('./iconSlide.png')
 const iconUser = require('./iconUser.png')
-const addButton = require('./addButton.png')
+const iconBlackHeart = require('./iconBlackHeart.png')
+const iconCreatePost = require('./iconCreatePost.png')
+const iconHome = require('./iconHome.png')
+const iconSearch = require('./iconSearch.png')
+const imageUser = require('./imageUser.png')
+const likeComment = require('./likeComment.png')
+const likedUsers = require('./likedUsers.png')
+const popupLikeComment = require('./popupLikeComment.png')
 export{
     avatar,iconCamera,iconDirectMessage,iconWatch,yourStory,
     story1,story2,story3,story4,iconMore,imageUpload,iconSave,
-    iconNumberOfPictures,iconHeart,iconComment,addButton,iconSlide,iconUser,
+    iconNumberOfPictures,iconHeart,iconComment,addButton,iconSlide,
+    iconUser, iconBlackHeart, iconCreatePost, iconHome,iconSearch,
+    imageUser, likeComment, likedUsers,popupLikeComment
 }
