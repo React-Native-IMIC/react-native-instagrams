ea6bf9f4cc7f87280a25adefebfc293ff21c610a
Nguyene <thaonguyenngoc22021@gmail.com>
2020-08-29 13:23:30 +0700

    Init Project

---
+++ NguyenNgocThaoNguyen/instagrams/assets/images/index.js
@@ -0,0 +1,31 @@
+const photo = require( './photo.png');
+const send = require( './send.png');
+const TV = require( './TV.png');
+const avatar = require( './avatar.png');
+const avatar2 = require( './Avatar2.png');
+const more = require( './More.png');
+const image = require( './Image.png');
+const slide = require( './SliderCounter.png');
+const fill1 = require( './Fill1.png');
+const combine = require( './CombinedShape.png');
+const fill2 = require( './Fill2.png');
+const fill346 = require( './Fill346.png');
+const dots = require( './SliderDots.png');
+const users = require( './Users.png');
+const heart = require( './heart.png');
+const rectangle = require( './Rectangle.png');
+const hear = require( './Hear.png');
+const Image2 = require( './Image2.png');
+const search = require( './Search.png');
+const home = require( './Home.png');
+const create = require( './CreatePost.png');
+const rectangle24 = require( './Rectangle24.png');
+const stories4 = require( './Stories4.png');
+const stories = require( './Stories.png');
+const stories2 = require( './Stories2.png');
+const stories3 = require( './Stories3.png');
+const oval = require( './Oval.png');
+
+export {photo,send,TV,avatar,avatar2,more, image, slide,fill1,combine,fill2,fill346,dots, users,heart,rectangle,
+    hear,Image2,search,home,create,rectangle24,stories,stories2,stories3,stories4, oval
+};
