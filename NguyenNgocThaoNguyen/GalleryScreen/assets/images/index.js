7e0922fdecc92808548a1f17ea1fd8c919054c2c
Nguyene <thaonguyenngoc22021@gmail.com>
2020-08-29 15:52:00 +0700

    GalleryScreen

---
+++ NguyenNgocThaoNguyen/GalleryScreen/assets/images/index.js
@@ -0,0 +1,29 @@
+const shape = require( './Shape.png');
+const more = require( './More.png');
+const combine = require( './CombinedShape.png');
+const human = require( './HumanShape.png');
+const image1 = require( './image1.png');
+const image2 = require( './image2.png');
+const image3 = require( './image3.png');
+const home = require( './Home.png');
+const create = require( './CreatePost.png');
+const hear = require( './Hear.png');
+const search = require( './Search.png');
+const image = require( './Image.png');
+const rectangle = require( './Rectangle24.png');
+const image4 = require( './image4.png');
+const image5 = require( './image5.png');
+const image6 = require( './image6.png');
+const image8 = require( './image7.png');
+const image9 = require( './image8.png');
+const image10 = require( './image9.png');
+const image11 = require( './image10.png');
+const image12 = require( './image11.png');
+const image13 = require( './image12.png');
+const image14 = require( './image13.png');
+const image15 = require( './image14.png');
+const image7 = require( './image6+1.png');
+
+export {shape,more,combine, human, image1, image2,image3,home,create,search,hear,image,rectangle,
+    image4,image5,image6,image7,image8,image9,image10,image11,image12,image13,image14,image15
+};
